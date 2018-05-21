461426f8c1b9891e531213141fb1f9be96008e39
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-21 23:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,28 +13,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
- 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
-function sayHiToGrandma(lowercase) {
-  return `I can't hear you!`
-}
-function sayHiToGrandma(uppercase) {
-  return `YES INDEED!`
-}
 
 function sayHiToGrandma(string) {
- return `I love you, too.`
+ if`
  }
